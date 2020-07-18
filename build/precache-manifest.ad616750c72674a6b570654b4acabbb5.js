91c0dbb91cf690c241bafc5954100552c2894efa
Roberto Abril <>
2020-07-18 19:58:10 +0200

    First version.

---
+++ build/precache-manifest.ad616750c72674a6b570654b4acabbb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1851408cd7092b1dacd13b51e5ffab8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "060c2fb29abef1d09b2d",
+    "url": "/static/css/main.57106ece.chunk.css"
+  },
+  {
+    "revision": "cc8a73d6c78359d6136c",
+    "url": "/static/js/2.45d94653.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/static/js/2.45d94653.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "060c2fb29abef1d09b2d",
+    "url": "/static/js/main.94508cb9.chunk.js"
+  },
+  {
+    "revision": "a955223f051c0bad8a52",
+    "url": "/static/js/runtime-main.de5c983f.js"
+  }
+]);
